2e3e902df4c4a103ff7226d6ccd5dd1e9834e9b6
Blair Urish <burace17@hotmail.com>
2016-03-08 14:04:45 -0600

    starting to improve language detection method

--- include/langdetect.h
+++ include/langdetect.h
@@ -4,6 +4,42 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-void detect_language(const char text[]);
+// size of hash table
+#define HT_SIZE 1000
+
+// initial size of the language occurance array
+#define LANG_INITIAL_SIZE 50
+
+
+// used to keep track of the number of words encountered for each language
+typedef struct {
+	char lang_name[64];
+	unsigned int occurances;
+} LANG_T;
+
+
+// linked list structure to store a list of languages in key value pair 
+typedef struct lang_list {
+	char name[64];
+	struct lang_list* next;
+} LANG_LIST_T;
+
+// this structure associates a word (the key) with a (linked) list
+// of the languages it appears in
+typedef struct {
+	char key[64];
+	LANG_LIST_T* value;
+} KV_PAIR_T;
+
+// hash table to store everything 
+typedef struct list_cell {
+	KV_PAIR_T* pair;
+	struct list_cell* next;
+} LIST_CELL_T;
+
+void detect_language(char* text);
 #endif
--- src/langdetect.c
+++ src/langdetect.c
@@ -1,74 +1,266 @@
 #include "langdetect.h"
-#include <errno.h>
 
-int get_occurances(char* input[], int numWords, const char* filename) {
-	FILE* file = fopen(filename, "r");
-	int occurances = 0;
-	int i;
-	char buffer[BUFSIZ];
+// this array stores the number of words found for each language we process.
+static LANG_T* lang_occurances;
+static size_t lang_occurances_size = LANG_INITIAL_SIZE;
+static size_t numberOfLanguages;
 
-	if (file == NULL) {
-		char error[1024];
-		sprintf(error, "Could not open file: %d", errno);
-		display_dialog(error);		
-		return 0;
-	}
+// slightly modified string hashing function (sdbm) from: 
+// http://www.cse.yorku.ca/~oz/hash.html
+unsigned long hash(char *str) {
+	unsigned long hash = 0;
+	int c;				
+	while ((c = *str++))
+		hash = c + (hash << 6) + (hash << 16) - hash;			
+	return hash;
+}
 
-	while (fgets(buffer, sizeof(buffer), file)) {
-		size_t ln = strlen(buffer) - 1;
-		if (buffer[ln] == '\n')
-			buffer[ln] = '\0';
-		for (i = 0; i < numWords; i++) {
-			if (input[i] == NULL) {
-				break;
-			}
+// gets the index in the hash table of the given key
+static inline unsigned int hash_get_index(char* key) {
+	return hash(key) % HT_SIZE;	
+}
 
-			ln = strlen(input[i]) - 1;
-			
-			if (input[i][ln] == '\n') {
-				input[i][ln] = '\0';
-			}
-			
-#ifdef _WIN32
-			if (stricmp(input[i], buffer) == 0) {
-#else
-			if (strcasecmp(input[i], buffer) == 0) {
-#endif
-				occurances++;
-			}
+// returns the list cell that contains the target key
+LIST_CELL_T* hash_get_cell(char* key, LIST_CELL_T* list) {
+	LIST_CELL_T* p;
+	for (p = list; p != NULL; p = p->next) {
+		if (strcmp(p->pair->key, key) == 0) {
+			return p;	
 		}
 	}
-
-	fclose(file);
-	return occurances;
+	return NULL;
 }
 
-void detect_language(const char text[]) {
-	char* t = (char*)text;
-	char* pch;
-	char* inputWords[BUFSIZ];
-	char output[1024];
-	int numWords = 0;
-	int matchedEn, matchedEs, matchedFr, matchedDe;
+// puts the given key value pair in the given hash table
+void hash_put_kv(LIST_CELL_T** hash_table, KV_PAIR_T* kv) {
+	// find the index where this KV pair should go
+	unsigned int index = hash_get_index(kv->key);
+	// then find the cell
+	LIST_CELL_T* cell = hash_get_cell(kv->key, hash_table[index]);
+	if (cell == NULL) {
+		// if there's nothing in this cell, allocate some memory for it
+		LIST_CELL_T* cell = calloc(1, sizeof(LIST_CELL_T));
+		cell->pair = kv;
+		cell->next = hash_table[index];
+		hash_table[index] = cell;	
+	}
+	else {
+		// this key is already in the hash table
+		printf("error: key collision: %s", kv->key);
+	}
+}
 
-	pch = strtok(t, " ");
-	while (pch != NULL) {
-		inputWords[numWords] = pch;
-		numWords++;
-		pch = strtok(NULL, " ");	
+// frees all of the objects associated with a given hash table
+void hash_free(LIST_CELL_T** hash_table) {
+	size_t i;
+	for (i = 0; i < HT_SIZE; i++) {
+		LIST_CELL_T* ph = hash_table[i];
+		LIST_CELL_T* p = hash_table[i];
+		while (p != NULL) {
+			LANG_LIST_T* qh = p->pair->value;
+			LANG_LIST_T* q = p->pair->value;
+			while (q != NULL) { // also deallocate the language linked list
+				q = q->next;
+				free(qh);
+				qh = q;
+			}
+
+			// deallocate key value pair
+			free(p->pair);
+
+			// finally, deallocate this linked list cell
+			p = p->next;
+			free(ph);
+			ph = p;
+		}
+
+	}
+	free(hash_table);
+}
+
+// finds the given word in the given hash table
+// returns the key value pair with the key equal to the given word.
+KV_PAIR_T* find_word(LIST_CELL_T** hash_table, char* word) {
+	// get the index in the hash table where this word should be located
+	unsigned int index = hash_get_index(word);
+	// get the list cell that contains the target word and return it if found
+	LIST_CELL_T* cell = hash_get_cell(word, hash_table[index]);
+
+	if (cell != NULL) {
+		return cell->pair;
+	}
+	return NULL;
+} 
+
+// adds all of the stop words in the given file to the hash table
+// sets errno if a problem occurs
+void process_language(LIST_CELL_T** hash_table, char* name) {
+	FILE* fp;
+	char line[BUFSIZ];
+	char* language;
+	LANG_T lang;
+	KV_PAIR_T* kv;
+	LANG_LIST_T* cell;
+	LANG_T* tmp;
+
+	if ((fp = fopen(name, "r")) != NULL) {
+		// the language name will be everything before the .
+		language = strtok(name, ".");
+
+		// add this language to the language occurance array
+		strncpy(lang.lang_name, language, sizeof(lang.lang_name));	
+		lang.occurances = 0;
+
+		if (numberOfLanguages == lang_occurances_size) {
+			// if the array is full, double its size
+			tmp = realloc(lang_occurances, lang_occurances_size * 2 * sizeof(LANG_T));
+			if (tmp != NULL) {
+				lang_occurances = tmp;
+  			lang_occurances_size *= 2;
+			}
+			else {
+				fclose(fp);
+				errno = ENOMEM;
+				return;
+			}
+		}
+		// add this language to the occurance array
+		lang_occurances[numberOfLanguages++] = lang;
+
+		// read each line of the file
+		while (fgets(line, sizeof(line), fp)) {
+			// strip newline char from input line
+			line[strcspn(line, "\n")] = '\0';
+
+			// locate this word in the hash table (it may or may not be there already)
+			kv = find_word(hash_table, line);
+		
+		  // allocate some memory to store this language in the language list	for this word
+			cell = calloc(1, sizeof(LANG_LIST_T));
+			strncpy(cell->name, language, sizeof(cell->name));
+			
+			if (kv != NULL) {
+				// word is in hash table
+				// add this language to the front of the linked list
+				cell->next = kv->value;
+				kv->value = cell;
+			} else {
+				// this word isn't in the hash table, so we need to allocate some memory for its
+				// KV pair. 
+				kv = calloc(1, sizeof(KV_PAIR_T));
+				// copy the language name to the key 
+				strncpy(kv->key, line, sizeof(kv->key));
+				// value is the linked list of languages
+				kv->value = cell;
+				hash_put_kv(hash_table, kv);
+			}
+
+		}	
+	} else {
+		errno = ENOENT;
+		return;
+	}
+	fclose(fp);
+	errno = 0;
+}
+
+// this function is used by the analyze() function to 
+// walk through the language linked list associated with each
+// encountered word. this handles cases where a word is associated
+// with multiple languages
+void update_occurances(LANG_LIST_T* list) {
+	LANG_LIST_T* p;
+	size_t i;	
+	for (p = list; p != NULL; p = p->next) {
+		for (i = 0; i < numberOfLanguages; i++) {
+			if (strcmp(p->name, lang_occurances[i].lang_name) == 0) {
+				lang_occurances[i].occurances++;
+				break;
+			}	
+		}	
+	}
+}
+
+// this function takes in the word->language hash table and
+// a file to analyze. it updates the lang_occurances array
+// with the number of time each encountered word appears in the
+// languages that we can track. 
+void analyze(LIST_CELL_T** hash_table, FILE* fp) {
+	char buffer[BUFSIZ];
+	char* word;
+	KV_PAIR_T* kv; 
+	while (fgets(buffer, sizeof(buffer), fp)) {
+		buffer[strcspn(buffer, "\n")] = '\0';
+		word = strtok(buffer, " ");
+		while (word != NULL) {
+			kv = find_word(hash_table, word);		
+			if (kv != NULL) {
+				update_occurances(kv->value);			
+			}
+			word = strtok(NULL, " ");	
+		}	
+	}	
+}
+
+void detect_language(char* text) {
+	DIR* stop_files;
+	struct dirent* dir;
+	LIST_CELL_T** hash_table;
+	unsigned int max = 0;
+	size_t max_index = 0;
+	size_t i;
+	FILE* text_stream;
+	char output[BUFSIZ];
+
+	// Create hash table 
+	hash_table = calloc(HT_SIZE, sizeof(LIST_CELL_T*));
+
+	// Create language occurance array
+	lang_occurances = calloc(LANG_INITIAL_SIZE, sizeof(LANG_T));
+
+	// open the stop files directory and then try to process each file
+	if ((stop_files = opendir("STOP_FILES/") != NULL)) {
+		chdir("STOP_FILES/");
+		while ((dir = readdir(stop_files)) != NULL) {
+			if (dir->d_type == DT_REG) { 
+				process_language(hash_table, dir->d_name);
+			  if (errno == ENOMEM) {
+					// terminate if language processing failed due to lack of memory
+					printf("error: out of memory\n");
+					hash_free(hash_table);
+					free(lang_occurances);
+					closedir(stop_files);
+					return 1;
+				} else if (errno == ENOENT) {
+					// skip file if we can't open it
+					printf("error: could not open %s, skipping file\n", dir->d_name);
+				}
+			}
+		}
+	} else {
+		perror("error: could not open stop file directory");
+		return 1;
 	}
 
-	matchedEn = get_occurances(inputWords, numWords, "./en.dic");
-	matchedEs = get_occurances(inputWords, numWords, "./es.dic");
-	matchedFr = get_occurances(inputWords, numWords, "./fr.dic");
-	matchedDe = get_occurances(inputWords, numWords, "./de.dic");
 
-	sprintf(output, "English: %d/%d (%g%%)\n Spanish: %d/%d (%g%%)\n French: %d/%d (%g%%)\n German: %d/%d (%g%%)\n",
-			matchedEn, numWords, floor((matchedEn/(double)numWords)* 100),
-			matchedEs, numWords, floor((matchedEs/(double)numWords)* 100),
-			matchedFr, numWords, floor((matchedFr/(double)numWords)* 100),
-			matchedDe, numWords, floor((matchedDe/(double)numWords)* 100));
+	text_stream = fmemopen(text, strlen(text), "r");
+	analyze(hash_table, text_stream);
+	fclose(text_stream);
 
+	// find out which language has the most occurances
+	for (i = 0; i < numberOfLanguages; i++) {
+		 if (lang_occurances[i].occurances > max) {
+		 		max = lang_occurances[i].occurances;
+				max_index = i;
+		 }	
+	}
+
+	// finally, print out the language with the most occurances
+	sprintf(output, "%s\n", lang_occurances[max_index].lang_name);
 	display_dialog(output);
-}
 
+	// clean up
+	hash_free(hash_table);
+	free(lang_occurances);
+	closedir(stop_files);
+}
