854dd430e82c6a82ccb3dadafba4934ae21f87ec
blair <burace17@hotmail.com>
2016-03-10 00:40:11 -0600

    noticed a possible bug when loading a different stop words directory

--- src/langdetect.c
+++ src/langdetect.c
@@ -200,6 +200,8 @@ void analyze(LIST_CELL_T** hash_table, char* text) {
 }
 
 void cleanup() {
+	numberOfLanguages = 0;
+	lang_occurance_size = LANG_INITIAL_SIZE;
 	if (word_dictionary != NULL) {
 		hash_free(word_dictionary);
 	}
