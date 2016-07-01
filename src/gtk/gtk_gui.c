3197f6bc81eda35d87f58ceb4f9c10e94df9db99
burace17 <burace17@gmail.com>
2016-07-01 18:50:46 -0500

    added gtk 3 support. it is now the default and gtk 2 is used as a fallback

--- src/gtk/gtk_gui.c
+++ src/gtk/gtk_gui.c
@@ -16,8 +16,8 @@ void select_button_click(GtkWidget* widget, gpointer data) {
 	dialog = gtk_file_chooser_dialog_new("Open Stop Word Folder",
 																				GTK_WINDOW(window),
 																				GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER,
-																				GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
-																				GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT, NULL);
+																				"_Cancel", GTK_RESPONSE_CANCEL,
+																				"_Open", GTK_RESPONSE_ACCEPT, NULL);
 
 	if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
 		filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
